190d2c80bc7d9d3f4ed27aa6bea526d285dad056
DomoMyles <47869842+DomoMyles@users.noreply.github.com>
2022-10-10 15:57:19 -0400

    Finishing touches to index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {
 // connects to mysql database
 server.connect(function (err) {
   if (err) throw err;
-  console.log('Server Connected');
+  console.log('---Server Connected---');
   // add the function that starts that communicates with mysql
   log()
 });
@@ -77,7 +77,7 @@ const db = mysql.createConnection({
     port: 3306,
     database: "company_db"
   },
-  console.log(`Connected to the movies_db database.`)
+  console.log(`---Connected to the database---`)
 );
 
 const queryPromise = (statement, params) => {
@@ -103,7 +103,7 @@ function addDepts() {
     .then(function (depd) {
       server.query(`INSERT INTO department (name) VALUES ('${depd.deptname}')`, function (err) {
         if (err) throw err
-        console.log("Added to departments")
+        console.log("---Added department to database---")
         log()
       })
     })
@@ -129,15 +129,14 @@ const addRoles = async () => {
         const results = await queryPromise(
           "select department.name AS name, department.id as value from department",
         )
-        console.log(results, answers)
         return results;
       }
     }])
-    /////////send it to the mysql database
+    //send to database
     .then(function (Answers) {
       server.query(`INSERT INTO role (title, salary, department_id) VALUES ('${Answers.rolename}', '${Answers.roleSalary}', '${Answers.deptName}')`, function (err, result) {
         if (err) throw err;
-        console.log("---Added role---")
+        console.log("---Added role to database---")
         log()
       })
     })
@@ -145,7 +144,7 @@ const addRoles = async () => {
 
 
 
-///add exmployeees
+///add employees
 const addEmployees = async () => {
   const response = await inquirer.prompt([{
       type: "input",
@@ -175,14 +174,13 @@ const addEmployees = async () => {
           "select employeedb.first_name AS name, employeedb.id AS value from employeedb",
           `%${answers.manager}%`
         );
-        console.log(answers)
         return results;
       }
     }])
     .then(function (Answers) {
       server.query(`INSERT INTO employeedb (first_name, last_name, role_id, manager_id) VALUES ('${Answers.firstName}', '${Answers.lastName}', '${Answers.role}', '${Answers.manger}')`, function (err, result) {
         if (err) throw err;
-        console.log("Added role")
+        console.log("---Added employee to database---")
         log()
       })
     })
@@ -225,15 +223,13 @@ const update = async () => {
           "select role.title AS name, role.id AS value from role",
         )
         return results;
-        console.log(results)
       }
     }])
     .then(function (Answers) {
-      console.log(Answers)
       server.query(`UPDATE employeedb SET role_id = '${Answers.role}' WHERE id = ${Answers.Employe};`, function (err, result) {
         if (err) throw err;
 
-        console.log("Updated Role")
+        console.log("---Updated Role---")
         log()
       })
     })
@@ -247,7 +243,7 @@ const update = async () => {
 
 
 /////DISPLAY EVERYTHING
-
+//display departments
 function displayDepts() {
   server.query('SELECT * FROM company_db.department', function (err, results) {
     if (err) throw err
@@ -255,6 +251,8 @@ function displayDepts() {
     log()
   })
 }
+
+//display role
 async function displayRole() {
   const varid = await queryPromise(`SELECT role.id, role.title, department.name AS department, role.salary
                     FROM department
@@ -263,11 +261,14 @@ async function displayRole() {
   log()
 }
 
+
+//SEND STUFF TO THE EMPLOYEE DB
 async function displayEmployees() {
   const employeTable = await queryPromise(`SELECT e.id,
                                                   e.first_name,
                                                   e.last_name,
                                                   role.title,
+                                                  department.name AS department,
                                                   role.salary,
                                                   CONCAT(m.first_name, ' ', m.last_name) AS 'Manager',
                                                   e.first_name AS 'first_name'
@@ -276,7 +277,9 @@ async function displayEmployees() {
                                                       ON role.id = e.role_id
                                                     INNER JOIN employeedb m
                                                       ON m.id = e.manager_id
-`) 
+                                                    INNER JOIN department
+                                                      ON department.id = role.department_id
+`)
   console.table(employeTable)
   log()
 }
